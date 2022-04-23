ac79aef314e8dc7f42781eb02e1c2463cf4e4318
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-04-23 23:04:57 +0100

    paragraph

--- src/components/UI/Button/Demo/DemoOutput.js
+++ src/components/UI/Button/Demo/DemoOutput.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Paragraph } from "../../../Paragraph";
 
 const DemoOutput = (props) => {
-  return <p>{props.show ? "This is New!" : ""}</p>;
+  return <Paragraph>{props.show ? "This is New!" : ""}</Paragraph>;
 };
 
 export default DemoOutput;
